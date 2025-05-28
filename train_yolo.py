092cae2fedbb3514a7cd9be14b129d7e709458ed
Monisha-RK10 <monisha.rk2415@gmail.com>
2025-05-28 10:46:26 +0200

    Delete train_yolo.py

--- train_yolo.py
+++
@@ -1,8 +0,0 @@
-from ultralytics import YOLO
-
-# Step 3: Load model & yaml to train the model.
-# Train model by setting epoch, image size, batch size.
-
-model = YOLO('yolov8n.yaml')  # or 'yolov8s.yaml'
-model.train(data='/content/drive/MyDrive/TrafficSignal/data.yaml',
-            epochs=50, imgsz=640, batch=16)
