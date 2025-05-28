f8aa42c3ed4d188a30cdbe1863284f5b7e516431
Monisha-RK10 <monisha.rk2415@gmail.com>
2025-05-28 10:46:33 +0200

    Delete train_yolov8ns_seg.py

--- train_yolov8ns_seg.py
+++
@@ -1,5 +0,0 @@
-# Train YOLOv8ns model
-
-!yolo task=segment mode=train model=yolov8ns-seg.pt \
-    data=/content/drive/MyDrive/TrafficSignal/segmentation_updated/data.yaml \
-    epochs=250 imgsz=640 save=True
