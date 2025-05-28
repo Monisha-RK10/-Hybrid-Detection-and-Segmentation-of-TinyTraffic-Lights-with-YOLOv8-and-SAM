a33a5f5193285bfcdadbee4683417afeca3431c5
Monisha-RK10 <monisha.rk2415@gmail.com>
2025-05-28 11:26:42 +0200

    Update and rename train_yolov8ns_seg.py to train_yolov8n_seg.py

--- training_&_inference_scripts/train_yolov8n_seg.py
+++ training_&_inference_scripts/train_yolov8n_seg.py
@@ -2,6 +2,6 @@
 
 from ultralytics import YOLO
 
-!yolo task=segment mode=train model=yolov8ns-seg.pt \
+!yolo task=segment mode=train model=yolov8n-seg.pt \
     data=/content/drive/MyDrive/TrafficSignal/segmentation_updated/data.yaml \
     epochs=250 imgsz=640 save=True
