6c17f69443d36cc75aaa5de8fffb632257f5ea65
Monisha-RK10 <monisha.rk2415@gmail.com>
2025-05-30 09:40:07 +0200

    Update train_yolov8s_seg.py

--- training_&_inference_scripts/train_yolov8s_seg.py
+++ training_&_inference_scripts/train_yolov8s_seg.py
@@ -1,6 +1,7 @@
-# Train YOLOv8s segmenter model
-
 from ultralytics import YOLO
+
+# Step 8: Load model & yaml to train the model.
+# Train YOLOv8n segmenter model
 
 !yolo task=segment mode=train model=yolov8s-seg.pt \
     data=/content/drive/MyDrive/TrafficSignal/segmentation_updated/data.yaml \
