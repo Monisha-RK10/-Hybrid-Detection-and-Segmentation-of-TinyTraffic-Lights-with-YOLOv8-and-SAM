024d317d25f747beb5f63fb9aba652d7c32a4dd2
Monisha-RK10 <monisha.rk2415@gmail.com>
2025-06-06 21:03:42 +0200

    Update inference_and_export.py

--- training_&_inference_scripts/inference_and_export.py
+++ training_&_inference_scripts/inference_and_export.py
@@ -6,12 +6,12 @@ from google.colab import files
 model = YOLO("/content/drive/MyDrive/TrafficSignal/best.pt")
 results = model.predict(source="/content/drive/MyDrive/TrafficSignal/test/images", save=True, conf=0.25)
 
-# Step 5: Zip & download results
+# Zip & download results
 
 !zip -r results.zip runs/detect/predict/
 files.download("results.zip")
 
-# Step 6: Export for deployment
+# Export for deployment
 
 model.export(format="onnx")
 files.download("best.onnx")
