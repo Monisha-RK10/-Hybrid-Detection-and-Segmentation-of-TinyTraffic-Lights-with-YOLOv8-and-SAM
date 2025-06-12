bb84605fb0352af8c7518b3383f7f15e831a63a5
Monisha-RK10 <monisha.rk2415@gmail.com>
2025-06-12 03:10:30 +0200

    Update yolo_seg_label_gen.py

--- utils/yolo_seg_label_gen.py
+++ utils/yolo_seg_label_gen.py
@@ -3,7 +3,7 @@ import os
 from pathlib import Path
 
 # Step 5: Generate YOLO segmentation labels for images.
-# YOLOv8 segmentation format: (class cx cy w h [x1 y1 x2 y2 ...]), Not binary masks, but polygon-based labels.
+# YOLOv8 segmentation format: (class cx cy w h [x1 y1 x2 y2 ...]). Not binary masks, but polygon-based labels.
 
 # Paths
 json_path = Path("/content/drive/MyDrive/TrafficSignal/segmentation_updated/annotation.json")
@@ -21,27 +21,27 @@ image_id_to_filename = {img['id']: img['file_name'] for img in coco['images']}
 image_id_to_size = {img['id']: (img['width'], img['height']) for img in coco['images']}
 
 # Create annotation lists grouped by image_id
-annotations_per_image = {} # Approach 2: Pre-group annotations: O(1) lookup per image (very fast), no repeated filtering/searching.
+annotations_per_image = {}                                                                    # Approach 2: Pre-group annotations: O(1) lookup per image (very fast), no repeated filtering/searching.
 for ann in coco['annotations']:
     image_id = ann['image_id']
-    annotations_per_image.setdefault(image_id, []).append(ann) # setdefault(k, v) checks if k exists in the dictionary, {1: [ann1, ann2],  2: [ann3],  3: [ann4, ann5, ann6],...}
+    annotations_per_image.setdefault(image_id, []).append(ann)                                # setdefault(k, v) checks if k exists in the dictionary, {1: [ann1, ann2],  2: [ann3],  3: [ann4, ann5, ann6],...}
 
 # Map category_id to zero-based index if needed
 categories = coco['categories']
-category_id_map = {cat['id']: i for i, cat in enumerate(categories)}  # cat: each dictionary inside categories, for ex categories = [{"id": 2, "name": "Red"}, {"id": 5, "name": "Green"}] result: category_id_map = {2: 0, 5: 1}
+category_id_map = {cat['id']: i for i, cat in enumerate(categories)}                          # cat: each dictionary inside categories, for ex categories = [{"id": 2, "name": "Red"}, {"id": 5, "name": "Green"}] result: category_id_map = {2: 0, 5: 1}
 
 # Convert annotations to YOLOv8 segmentation format
 for image_id, file_name in image_id_to_filename.items():
     width, height = image_id_to_size[image_id] # (W, H) lookup
-    annotations = annotations_per_image.get(image_id, []) # All anns for this image
+    annotations = annotations_per_image.get(image_id, [])                                      # All anns for this image
     lines = []
 
     for ann in annotations:
         category_id = ann['category_id']
-        class_id = category_id_map[category_id]  # Map to YOLO class ID
-        segmentation = ann['segmentation'][0]  # Only one polygon expected, to avoid multiple disconnected part
-        xs = segmentation[0::2] # splits the polygon list into X coordinates (every other value starting from index 0)
-        ys = segmentation[1::2] # splits the polygon list into Y coordinates (every other value starting from index 1)
+        class_id = category_id_map[category_id]                                                 # Map to YOLO class ID
+        segmentation = ann['segmentation'][0]                                                   # Only one polygon expected, to avoid multiple disconnected part
+        xs = segmentation[0::2]                                                                 # Splits the polygon list into X coordinates (every other value starting from index 0)
+        ys = segmentation[1::2]                                                                 # Splits the polygon list into Y coordinates (every other value starting from index 1)
 
         # Compute bounding box (YOLO format)
         x_min, y_min = min(xs), min(ys)
@@ -55,14 +55,14 @@ for image_id, file_name in image_id_to_filename.items():
         # if i % 2 == 0: means X coordinate (even indices), divide it by width.
         # if i % 2 != 0: means Y coordinate (odd indices), divide it by height.
         norm_seg = [str(round(x / width, 6)) if i % 2 == 0 else str(round(x / height, 6))
-                    for i, x in enumerate(segmentation)] # Round to 6 decimal places, and convert to string 
+                    for i, x in enumerate(segmentation)]                                         # Round to 6 decimal places, and convert to string 
 
         line = f"{class_id} {x_center:.6f} {y_center:.6f} {w:.6f} {h:.6f} " + " ".join(norm_seg) # Join elements of the list norm_seg with a space between them.
         lines.append(line)
 
     # Write label file
-    label_file = labels_dir / (Path(file_name).stem + ".txt") # returns the file name without the extension.
+    label_file = labels_dir / (Path(file_name).stem + ".txt")                                    # Returns the file name without the extension.
     with open(label_file, "w") as f:
         f.write("\n".join(lines))
 
-labels_dir.exists(), len(list(labels_dir.glob("*.txt")))  # Return status and file count
+labels_dir.exists(), len(list(labels_dir.glob("*.txt")))                                         # Return status and file count
