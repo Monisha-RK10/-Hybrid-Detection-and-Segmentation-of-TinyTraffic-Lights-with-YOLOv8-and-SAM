14c7621848e296fc5decde44f763d0aa418088ff
Monisha-RK10 <monisha.rk2415@gmail.com>
2025-06-14 01:01:37 +0200

    Update train_yolo.py

--- training_&_inference_scripts/train_yolo.py
+++ training_&_inference_scripts/train_yolo.py
@@ -3,5 +3,5 @@ from ultralytics import YOLO
 # Step 3: Load model & yaml to train the model.
 # Train model by setting epoch, image size, batch size.
 
-model = YOLO('yolov8n.yaml')  # or 'yolov8s.yaml'
+model = YOLO('yolov8n.yaml')                        # or 'yolov8s.yaml'
 model.train(data='/content/drive/MyDrive/TrafficSignal/data.yaml', epochs=100, patience=20,  imgsz=640, batch=16)
