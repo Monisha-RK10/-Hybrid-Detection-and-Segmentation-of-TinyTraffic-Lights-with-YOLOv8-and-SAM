35c3cb613446f5c5b1a3770b777c086b79e3d668
Monisha-RK10 <monisha.rk2415@gmail.com>
2025-06-14 02:18:17 +0200

    Update inference_and_export.py

--- training_&_inference_scripts/inference_and_export.py
+++ training_&_inference_scripts/inference_and_export.py
@@ -1,7 +1,7 @@
+# Step 4: Load model & run prediction
+
 from ultralytics import YOLO
 from google.colab import files
-
-# Step 4: Load model & run prediction
 
 model = YOLO("/content/drive/MyDrive/TrafficSignal/best.pt")
 results = model.predict(source="/content/drive/MyDrive/TrafficSignal/test/images", save=True, conf=0.25)
